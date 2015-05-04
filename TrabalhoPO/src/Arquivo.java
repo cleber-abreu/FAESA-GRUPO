a4c8803e891ad8e3e3f4fcf7841835a1dbc9074f
cleberjunio <cleberjunio@MESPF224467.AGU.GOV.BR>
2015-05-04 15:35:41 -0300

    Adicionando Classe Arquivo Contendo os metodos: ler // para carregar o vetor de Item lerCpf gravar

---
+++ TrabalhoPO/src/Arquivo.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Arquivo {
+	
+	private static final String ENTRADA = "./Dados/Entrada/";
+	private static final String SAIDA = "./Dados/Saida/";
+	
+	public static void ler(Item[] vetorDados, String arquivo) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(new File(ENTRADA + arquivo)));
+			String linha = "";
+			String campo[];
+			int i = 0;
+			
+			while ((linha = in.readLine()) != null) {
+				campo = linha.split(";");
+				vetorDados[i] = new Item(campo[0], campo[1], campo[2], campo[3]);
+				i++;
+			}
+			
+			in.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void lerCpf(String[] VetorCPF) {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(ENTRADA + "cpf.txt"));
+			String linha = "";
+			int i = 0;
+			
+			while ((linha = in.readLine()) != null) {
+				VetorCPF[i] = linha;
+				i++;
+			}
+			in.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void gravar(Item[] vetorDados, String arquivo) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(new File(SAIDA + arquivo)));
+			for (int i = 0; i < vetorDados.length; i++) {
+				out.write(vetorDados[i].getCpf() + ";" 
+						+ vetorDados[i].getNome() + ";" 
+						+ vetorDados[i].getData() + ";" 
+						+ vetorDados[i].getValor());
+				out.newLine();
+			}
+			out.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
